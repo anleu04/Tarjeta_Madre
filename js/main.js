17a99c7cb5f3e9e69bb533e33b6eebe61f0167f8
Anleu4 <josueanleu4@gmail.com>
2024-08-07 10:40:22 -0600

    grid en la foto

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let tarjeta = document.querySelector("#root");
+
+tarjeta.innerHTML = `
+<div class="padre">
+    <div class="grid">
+        <div class="header"><p>Placa Madre</p></div>
+        <div class="foto">
+            <div class="comlumna1"></div>
+            <div class="comlumna2">
+                <div class="gridcolum2"></div>
+                <img src="https://raw.githubusercontent.com/anleu04/img/main/Placa.jpg" alt="">
+            </div>
+            <div class="comlumna3"></div>
+        </div>
+    </div>
+</div>
+`;
