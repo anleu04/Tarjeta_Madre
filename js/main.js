eebbfdabe58676ebeb458e832375a485903815a8
Anleu4 <josueanleu4@gmail.com>
2024-08-25 13:42:41 -0600

    Boton descarga

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ tarjeta.innerHTML = `
                             <button class="open-modal boton1">RAM</button>
                             <div class="modal">
                                 <div class="modal-content">
-                                    <button class="download-btn">Descargar JPG</button>
+                                    <button class="download-btn">⏬</button>
                                     <span class="close">&times;</span>
                                     <h1>Ranuras De RAM</h1>
                                     <p>Una ranura de RAM, también conocida como ranura de memoria o zócalo de memoria, es un conector físico en una computadora tarjeta madre Diseñado para contener y conectar módulos de memoria de acceso aleatorio (RAM). Estas ranuras permiten a los usuarios ampliar la capacidad de memoria del sistema agregando módulos RAM adicionales, lo que mejora el rendimiento y las capacidades multitarea. Existen diferentes tipos de ranuras de RAM, como DDR4 y DDR5, cada una compatible con módulos de RAM específicos. Las placas base modernas suelen venir con entre 2 y 4 ranuras de memoria, lo que también determina qué tipo de RAM utiliza la computadora.</p>
@@ -28,7 +28,7 @@ tarjeta.innerHTML = `
                             <button class="open-modal boton2">SOCKET</button>
                             <div class="modal">
                                 <div class="modal-content">
-                                    <button class="download-btn">Descargar JPG</button>
+                                    <button class="download-btn">⏬</button>
                                     <span class="close">&times;</span>
                                     <h1>SOCKET</h1>
                                     <p>El socket o zócalo es la zona de la placa base donde se instala la CPU, posicionada al costado de los slots de memoria RAM y justo encima de las ranuras PCI-Express. Intel y AMD han ido desarrollando varios sockets conforme iban lanzando familias de procesadores.
@@ -46,7 +46,7 @@ tarjeta.innerHTML = `
                             <button class="open-modal boton3">BIOS</button>
                             <div class="modal">
                                 <div class="modal-content">
-                                    <button class="download-btn">Descargar JPG</button>
+                                    <button class="download-btn">⏬</button>
                                     <span class="close">&times;</span>
                                     <h1>BIOS</h1>
                                     <p>El BIOS (sistema básico de entrada y salida) es el programa que utiliza el microprocesador de una computadora para iniciar el sistema informático después de encenderlo. También administra el flujo de datos entre el sistema operativo (OS) de la computadora y los dispositivos conectados, como el disco duro, el adaptador de video, el teclado, el mouse y la impresora.</p>
@@ -62,7 +62,7 @@ tarjeta.innerHTML = `
                             <button class="open-modal boton4">24 PINES</button>
                             <div class="modal">
                                 <div class="modal-content">
-                                    <button class="download-btn">Descargar JPG</button>
+                                    <button class="download-btn">⏬</button>
                                     <span class="close">&times;</span>
                                     <h1>24 PINES</h1>
                                     <p>Este es el conector de alimentación principal para la placa base. Actualmente, este conector ya suele ser un conector único de 24 pines, pero en algunos casos tenemos un conector de 20 pines y un extra de 4 pines. El motivo de este segundo tipo de conector es que algunas placas base antiguas usaban un conector de 20 pines, pero está en desuso y por este motivo ya casi siempre es un conector único de 24 pines y ya está.</p>
@@ -78,7 +78,7 @@ tarjeta.innerHTML = `
                             <button class="open-modal boton5">RANURA PCI</button>
                             <div class="modal">
                                 <div class="modal-content">
-                                    <button class="download-btn">Descargar JPG</button>
+                                    <button class="download-btn">⏬</button>
                                     <span class="close">&times;</span>
                                     <h1>RANURA PCI</h1>
                                     <p>Una ranura PCI, por lo tanto, es un espacio abierto en una placa madre (también conocida como placa base, tarjeta madre, motherboard o mainboard) que se usa para conectar tarjetas de expansión. Antes de avanzar, es importante aclarar varios conceptos.
@@ -99,7 +99,7 @@ tarjeta.innerHTML = `
                             <button class="open-modal boton6">CONECTOR PROCESADOR</button>
                             <div class="modal">
                                 <div class="modal-content">
-                                    <button class="download-btn">Descargar JPG</button>
+                                    <button class="download-btn">⏬</button>
                                     <span class="close">&times;</span>
                                     <h1>CONECTOR PROCESADOR</h1>
                                     <p>Un conector de alimentación de CPU de 4 pines es el conector estándar para la mayoría de las CPU. Tiene dos cables amarillos y dos cables negros, y se conecta a un zócalo de 4 pinos en la placa base . Un conector de 4 pines puede entregar hasta 192 vatios de potencia, que es suficiente para la mayoría de las CPU.</p>
@@ -116,7 +116,7 @@ tarjeta.innerHTML = `
                             <button class="open-modal boton7">PUERTOS</button>
                             <div class="modal">
                                 <div class="modal-content">
-                                    <button class="download-btn">Descargar JPG</button>
+                                    <button class="download-btn">⏬</button>
                                     <span class="close">&times;</span>
                                     <h1>PUERTOS</h1>
                                     <p>Los puertos de la placa madre son puntos de conexión externos e internos que permiten la comunicación entre la computadora y diversos dispositivos periféricos. Estos puertos son esenciales para la conectividad y funcionalidad del sistema. Algunos puertos son visibles en la parte trasera de la carcasa, como los USB, HDMI y Ethernet, mientras que otros, como los SATA y PCIe, están en el interior. Los puertos modernos han evolucionado para ofrecer mayores velocidades de transferencia de datos y mejor compatibilidad con una amplia gama de dispositivos, asegurando una integración perfecta con la tecnología actual.</p>
